471f948ebc2cf8f4a003b50bd5612c2d0ced3580
huangcaixia <euphoria.hcx@gmail.com>
2021-10-27 10:19:46 +0800

    first commit

---
+++ src/test/java/com/hcx/redistest/RedistestApplicationTests.java
@@ -0,0 +1,21 @@
+package com.hcx.redistest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class RedistestApplicationTests {
+
+    @Autowired
+    RedisTemplate<String,String> redisTemplate;
+
+    @Test
+    void contextLoads() {
+        redisTemplate.opsForValue().set("myKey","myValue");
+        System.out.println(redisTemplate.opsForValue().get("myKey"));
+
+    }
+
+}
